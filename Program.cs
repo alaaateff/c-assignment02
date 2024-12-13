b08de57ac79a4da235280fadffe861f4ad1a7852
alaaateff <alaaatef3200@gmail.com>
2024-12-13 22:44:27 +0200

    Q7)

--- Program.cs
+++ Program.cs
@@ -90,6 +90,19 @@ namespace secondproject
             }
             #endregion
 
+            #region question 7)
+            Console.WriteLine("Please enter 2 numbers");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int power = 1;
+            while (num2 != 0)
+            {
+                power *= num1;
+                num2--;
+            }
+            Console.WriteLine(power);
+            #endregion
+
 
 
 
